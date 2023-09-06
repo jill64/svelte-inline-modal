6f87c2bcccce8317c419aaf0d29b9dc0e25b4338
jill64 <intents.turrets0h@icloud.com>
2023-09-06 17:56:57 +0900

    add src

---
+++ src/lib/InlineModal.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  export let onClose: () => unknown = () => {}
+
+  const close = () => {
+    if (dialog?.open) {
+      onClose()
+    }
+    dialog?.close()
+  }
+
+  const open = () => {
+    dialog?.showModal()
+  }
+
+  let dialog: HTMLDialogElement | undefined
+</script>
+
+<slot {open} />
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<dialog bind:this={dialog} on:click|self={close} on:keypress>
+  <div>
+    <slot name="menu" {close} />
+  </div>
+</dialog>
+
+<style>
+  dialog {
+    border: none;
+    padding: 0;
+    background: transparent;
+    color: inherit;
+  }
+  dialog::backdrop {
+    background: rgba(0, 0, 0, 0.5);
+  }
+  dialog[open]::backdrop {
+    animation: fade 0.15s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import InlineModal from '$lib/index.js'
+</script>
+
+<h1>svelte-inline-modal</h1>
+<InlineModal onClose={() => console.log('Modal Closed')} let:open>
+  <button on:click={open}>Open</button>
+  <div slot="menu" let:close>
+    <ol>
+      <li>First</li>
+      <li>Second</li>
+      <li>Third</li>
+    </ol>
+    <button on:click={close}>Close</button>
+  </div>
+</InlineModal>
+
+<style>
+  :global(body) {
+    font-family: sans-serif;
+  }
+  @media (prefers-color-scheme: dark) {
+    :global(body) {
+      background: #333;
+      color: #fff;
+    }
+  }
+  div {
+    padding: 1rem;
+    background: #fff;
+    border-radius: 0.5rem;
+  }
+  @media (prefers-color-scheme: dark) {
+    div {
+      background: #333;
+    }
+  }
+</style>
