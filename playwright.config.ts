4fc79b3771fbb378b0c3511f88eb3dbdca8b99ba
jill64 <intents.turrets0h@icloud.com>
2023-10-21 22:56:15 +0900

    ref: npm scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'pnpm preview',
+    command: 'npm run preview',
     port: 4173
   },
   testDir: 'tests',
